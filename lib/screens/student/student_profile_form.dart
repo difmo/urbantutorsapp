206dc721c57ff75ab1b07936c2a5cea7213ea1e1
dineshkumartlbupi <dineshkumartlbupi@gmail.com>
2025-08-31 17:01:33 +0530

    save changes

--- lib/screens/student/student_profile_form.dart
+++ lib/screens/student/student_profile_form.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:urbantutorsapp/controllers/profile_update_controller.dart';
 import 'package:urbantutorsapp/models/profile_modals/student_profile_request_modal.dart';
 import 'package:urbantutorsapp/screens/controllers/lead_meta_controller.dart'
@@ -13,7 +12,6 @@ import 'package:urbantutorsapp/screens/controllers/masterdata_controller.dart';
 import 'dart:developer' as dev;
 
 import 'package:urbantutorsapp/utils/app_log.dart';
-import 'package:urbantutorsapp/utils/storage_helper.dart';
 
 class StudentProfileFormScreen extends StatefulWidget {
   const StudentProfileFormScreen({super.key});
@@ -165,7 +163,7 @@ class _StudentProfileFormScreenState extends State<StudentProfileFormScreen> {
       final backBase64 = await _fileToBase64(_backIdImage) ?? '';
 
       final request = StudentProfileUpdateRequest(
-        userId: getUserId(), // TODO: replace with actual logged-in user id
+        userId: 146, // TODO: replace with actual logged-in user id
         boardId: selectedBoardId!,
         courseId: selectedClassId!, // mapping "Class" -> courseId
         subjectId: selectedSubjectId!,
@@ -610,9 +608,5 @@ class _StudentProfileFormScreenState extends State<StudentProfileFormScreen> {
         ),
       ],
     );
-  }
-  
-  getUserId() {
-    return StorageService.getRole();
   }
 }
